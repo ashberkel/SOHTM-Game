5bfc2ea5241988b51066ab281a89dcd932ce2761
Ash Berkel <ashberkel@gmail.com>
2024-12-24 19:12:04 -0600

    changed UI and tested JS

--- myJS.js
+++ myJS.js
@@ -1,5 +1,11 @@
-function diaAdd() {
-    document.getElementById('diaBox').innerHTML += "What do you want?";
-}
+console.log('The JS has loaded!');
 
-console.log('Can the page see this?');
+const chars = [
+    {num: 0, name: "Frank", occupation: "Captain", desc: "Frank doesn't look happy to see you."},
+    {num: 1, name: "Rose", occupation: "Scientist", desc: "Rosie looks troubled."}];
+console.log(chars[0].desc);
+
+const items = [
+    {num: 0, name: "Box", desc: "It's worn and water damanged."},
+    {num: 1, name: "Key", desc: "This key is oddly shaped."}];
+console.log(items[1].desc);
