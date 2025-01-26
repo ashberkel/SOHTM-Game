620dd2c154213f6fbac54e9b5f33fde224a5287c
ashberkel <ashberkel@gmail.com>
2025-01-26 14:08:17 -0600

    all four direction movement

--- myJS.js
+++ myJS.js
@@ -35,11 +35,30 @@ function processClick() {
     setTimeout(function(){document.getElementById('diaBox').innerHTML = ""; diaButton.style.display = "none";},3000);
     //fix so that I calling a clearTimeout on the setTimeout 
 }
+// movement stuff
+const mapMoveJs = document.getElementById("mapMove");
 
-function leftMove() {
-    const mapMoveJs = document.getElementById("mapMove");
-    const curLft = getComputedStyle(mapMoveJs).getPropertyValue("left");
-    console.log(curLft);
-    var newLft = parseInt(curLft) - 825;
+function moveLeft() {
+    const curHor = getComputedStyle(mapMoveJs).getPropertyValue("left");
+    console.log(curHor);
+    var newLft = parseInt(curHor) - 825;
     mapMoveJs.style.left = newLft + "px";
+}
+function moveRight() {
+    const curHor = getComputedStyle(mapMoveJs).getPropertyValue("left");
+    console.log(curHor);
+    var newRght = parseInt(curHor) + 825;
+    mapMoveJs.style.left = newRght + "px";
+}
+function moveUp() {
+    const curVert = getComputedStyle(mapMoveJs).getPropertyValue("top");
+    console.log(curVert);
+    var newUp = parseInt(curVert) + 425;
+    mapMoveJs.style.top = newUp + "px";
+}
+function moveDown() {
+    const curVert = getComputedStyle(mapMoveJs).getPropertyValue("top");
+    console.log(curVert);
+    var newDown = parseInt(curVert) - 425;
+    mapMoveJs.style.top = newDown + "px";
 }
