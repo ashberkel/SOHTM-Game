7fac67ea71876b7ad644eac17a22a459392ad235
ashberkel <ashberkel@gmail.com>
2025-08-23 19:21:09 -0500

    added BGs, fixed coords

--- global.js
+++ global.js
@@ -3,58 +3,79 @@ console.log("Your MCChoice is: " + MCChoice);
 //movement logic
 $(document).ready(function()
 {
-  X = 0
-  Y = 0
-  Z = 0
-  coord = ""
-  room = ``
-  $(".movement").click(function()
-  {
-  	ID = this.id
-    if (ID == "moveRight")
-     {X += 1}
-    else if (ID == "moveLeft")
-     {X -= 1}
-    else if (ID == "moveUp")
-     {Y += 1}
-    else {Y -= 1}
-   coord = `${Z},${X},${Y}`
-   switch(coord) {
-   case "0,0,0":
-   room = "Bedroom"
-    $(".clickables").addClass("hide");
-    $(".clickables.MCRoom").removeClass("hide");
-   $("#moveUp, #moveDown, #moveLeft").addClass("hide");
-   $("#moveRight").toggleClass("hide");
-      $("#playView").css("background-image", "url('images/rooms/MCRoom.jpg')");
-   break;
-   case "0,1,0":
-   room = "Hallway"
-    $(".clickables").addClass("hide");
-    $(".clickables.MCHall").removeClass("hide");
-    $("#moveDown, #moveRight").addClass("hide");
-   $("#moveUp, #moveLeft").removeClass("hide");
-   $("#playView").css("background-image", "url('images/rooms/MCHall.jpg')");
-   break;
-   case "0,1,1":
-   room = "Elevator"
-    $(".clickables").addClass("hide");
-    $(".clickables.elevator").removeClass("hide");
-   $("#moveLeft").addClass("hide"); 
-   $("#moveDown"). removeClass("hide");
-      $("#playView").css("background-image", "url('images/rooms/Elev.jpg')");
-   break;
-      case "0,1,2":
-   room = "Floors"
-    $(".clickables").addClass("hide");
-    $(".clickables.Floors").removeClass("hide");
-   $("#moveUp").addClass("hide"); 
-   $("#moveDown"). removeClass("hide");
-      $("#playView").css("background-image", "url('images/rooms/Floors.jpg')");
-   break;
-   }
-     $("#coordinates").text("Coordinates:" + coord + "Room:" + room)
-  });
+    X = 0
+    Y = 0
+    Z = 3
+    coord = ""
+    room = ``
+    $(".movement").click(function()
+    {
+        ID = this.id
+        if (ID == "moveRight")
+            {X += 1}
+        else if (ID == "moveLeft")
+            {X -= 1}
+        else if (ID == "moveUp")
+            {Y += 1}
+        else {Y -= 1}
+        coord = `${Z},${X},${Y}`
+        switch(coord) {
+            case "3,0,0":
+            room = "Bedroom"
+            $(".clickables").addClass("hide");
+            $(".clickables.MCRoom").removeClass("hide");
+            $("#moveUp, #moveDown, #moveLeft").addClass("hide");
+            $("#moveRight").toggleClass("hide");
+            $("#playView").css("background-image", "url('images/rooms/MCRoom.jpg')");
+            break;
+            case "3,1,0":
+            room = "Hallway"
+            $(".clickables").addClass("hide");
+            $(".clickables.MCHall").removeClass("hide");
+            $("#moveDown, #moveRight").addClass("hide");
+            $("#moveUp, #moveLeft").removeClass("hide");
+            $("#playView").css("background-image", "url('images/rooms/MCHall.jpg')");
+            break;
+            case "3,1,1":
+            room = "Elevator3"
+            $(".clickables").addClass("hide");
+            $(".clickables.elevator").removeClass("hide");
+            $("#moveLeft").addClass("hide"); 
+            $("#moveDown, #moveUp"). removeClass("hide");
+            $("#playView").css("background-image", "url('images/rooms/Elev.jpg')");
+            break;
+            case "3,1,2":
+            room = "Floor3"
+            $(".clickables").addClass("hide");
+            $(".clickables.floors").removeClass("hide");
+            $("#moveUp").addClass("hide"); 
+            $("#moveDown"). removeClass("hide");
+            $("#playView").css("background-image", "url('images/rooms/Floors3-4O.jpg')");
+            break;
+            case "4,1,2":
+            room = "Floor4"
+            $(".clickables").addClass("hide");
+            $(".clickables.floors").removeClass("hide");
+            $("#moveUp").addClass("hide"); 
+            $("#moveDown"). removeClass("hide");
+            $("#playView").css("background-image", "url('images/rooms/Floors34-O.jpg')");
+            break;
+            case "4,1,1":
+            room = "Elevator4"
+            $(".clickables").addClass("hide");
+            $(".clickables.elevator").removeClass("hide");
+            $("#moveDown").addClass("hide");
+            $("#moveRight, #moveUp").removeClass("hide");
+            $("#playView").css("background-image", "url('images/rooms/Elev.jpg')");
+            break;
+            case "4,2,1":
+            room = "Hall4"
+            $("#moveRight").addClass("hide");
+            $("#moveLeft, #moveUp"). removeClass("hide");
+            $("#playView").css("background-image", "url('images/rooms/DiningHall.jpg')");
+        }
+        console.log("Coordinates:" + coord + " Room:" + room)
+    });
 });
 //Clickables
 $(".clickables").click(function() {
@@ -67,87 +88,112 @@ $(".clickables").click(function() {
                 $("#MCMirrorPic").attr("src", "images/characters/MCDog.gif");
             }
             dia = n.desc;
-                diaSplit(dia, () => {
+            diaSplit(dia, () => {
                 // Clear the image after diaSplit finishes
                 $("#MCMirrorPic").attr("src", "");
             });
             break; // stop searching after mirror
         }
-
-        if (thisItem.id === n.name) {
-            dia = n.desc;
+        if ($(this).hasClass("inactive")) {
+            dia = "Locked out. Looks like some floors are off-limits._Maybe they don't like the creaturefolk mixing with the elites.";
             diaSplit(dia);
-            break; // stop searching after match
-        }
-    }
-});
-//Inventory fill each slot
-$(".items").click(function()
-    {
-        thisItem = this;
-        console.log("Clicked on:", thisItem.id);
-        items.forEach(n =>
-        {
-            if (thisItem.id === n.name)
-            {
-                console.log("Comparing to item:", n.name);
-                n.inInv = true;
-                console.log("You got the" + n.name + items[0].inInv);
+            break;}
+            else if ($(this).hasClass("current")) {
+                dia = "I'm already on that floor.";
+                diaSplit(dia);
+                break;}
+            if (thisItem.id == "floor3")
+                {Z = 3;
+                $(".floors").removeClass("current");  
+                $("#floor3").addClass("current");
+                $("#playView").css("background-image", "url('images/rooms/Floors3-4O.jpg')");
+                break;}
+                else if (thisItem.id == "floor4")
+                    {Z = 4;
+                    $(".floors").removeClass("current");  
+                    $("#floor4").addClass("current");
+                    $("#playView").css("background-image", "url('images/rooms/Floors34-O.jpg')");
+                    break;}
+                else if (thisItem.id == "floorO1")
+                    {Z = 6;
+                    $(".floors").removeClass("current");  
+                    $("#floorO1").addClass("current");
+                    $("#playView").css("background-image", "url('images/rooms/Floors34O-.jpg')");
+                    break;}
+            if (thisItem.id === n.name) {
+                dia = n.desc;
+                diaSplit(dia);
+                break; // stop searching after match
+                }
             }
-            else {}
         });
-        $(".invItems").each(function()
+        //Inventory fill each slot
+        $(".items").click(function()
         {
-            if (!$(this).hasClass("filled"))
-            {
-                $(this).prepend(thisItem.id); 
-                $(this).addClass("filled");
-                const pickupSound = new Audio("assets/clickSoundPick.mp3");
-                pickupSound.play(); 
-                return false;
-            }
-        }
-        )
-    });
-    
-//Inventory remove each slot
-$("#door").click(function()
-            {
+            thisItem = this;
+            console.log("Clicked on:", thisItem.id);
+            items.forEach(n =>
+                {
+                    if (thisItem.id === n.name)
+                        {
+                        console.log("Comparing to item:", n.name);
+                        n.inInv = true;
+                        console.log("You got the" + n.name + items[0].inInv);
+                    }
+                    else {}
+                });
                 $(".invItems").each(function()
                 {
-                    if ($(this).hasClass("filled"))
-                    {
-                        $(this).empty("Yo!"); 
-                        $(this).removeClass("filled");
-                        const useSound = new Audio("assets/clickSoundUse.mp3");
-                        useSound.play();  
+                    if (!$(this).hasClass("filled"))
+                        {
+                        $(this).prepend(thisItem.id); 
+                        $(this).addClass("filled");
+                        const pickupSound = new Audio("assets/clickSoundPick.mp3");
+                        pickupSound.play(); 
                         return false;
                     }
                 }
-                )
-            });
-
-//Dialogue splitting
-function diaSplit(dia, onDone) {
-    const splitUp = dia.split("_");
-    var n = 0;
-    diaButton.style.display = "inline";
-    document.getElementById('diaBox').innerHTML = splitUp[n];
-    $('#moveIcons').addClass("hide");
-    $("#diaButton").off("click");
-    $("#diaButton").click(function(){
-        n += 1;
-        if (splitUp[n] != undefined)
+            )
+        });
+        
+        //Inventory remove each slot
+        $("#door").click(function()
         {
+            $(".invItems").each(function()
+            {
+                if ($(this).hasClass("filled"))
+                    {
+                    $(this).empty("Yo!"); 
+                    $(this).removeClass("filled");
+                    const useSound = new Audio("assets/clickSoundUse.mp3");
+                    useSound.play();  
+                    return false;
+                }
+            }
+        )
+    });
+    
+    //Dialogue splitting
+    function diaSplit(dia, onDone) {
+        const splitUp = dia.split("_");
+        var n = 0;
+        diaButton.style.display = "inline";
         document.getElementById('diaBox').innerHTML = splitUp[n];
-        }
-        else {
-        document.getElementById('diaBox').innerHTML = "";
-        diaButton.style.display = "none";
-        $('#moveIcons').removeClass("hide");
-        n = 0;
+        $('#moveIcons').addClass("hide");
+        $("#diaButton").off("click");
+        $("#diaButton").click(function(){
+            n += 1;
+            if (splitUp[n] != undefined)
+                {
+                document.getElementById('diaBox').innerHTML = splitUp[n];
+            }
+            else {
+                document.getElementById('diaBox').innerHTML = "";
+                diaButton.style.display = "none";
+                $('#moveIcons').removeClass("hide");
+                n = 0;
+            }
+            console.log(`Current n count is` + n);
+            if (onDone) onDone();
+        })
     }
-    console.log(`Current n count is` + n);
-    if (onDone) onDone();
-    })
-}
